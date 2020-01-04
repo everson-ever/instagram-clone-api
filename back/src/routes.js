a319a91e617cea7c86277675399a80478eb19f6a
Everson Silva <eversonsilva9799@gmail.com>
2020-01-04 13:14:34 -0300

    imported middleware file modified (permission.js)

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 const authMiddleware = require('./app/middlewares/auth');
-const adminMiddleware = require('./app/middlewares/admin');
+const permissionMiddleware = require('./app/middlewares/permission');
 const roles = require('./config/roles');
 
 const UserController = require('./app/controllers/UserController');
@@ -11,9 +11,8 @@ const PostController = require('./app/controllers/PostController');
 routes.post('/session', SessionController.store);
 routes.post('/register', UserController.store);
 
-routes.get('/users', [ authMiddleware, adminMiddleware(roles.admin) ], UserController.index);
+routes.get('/users', [ authMiddleware, permissionMiddleware(roles.admin) ], UserController.index);
 routes.get('/users/:id', authMiddleware, UserController.get);
-routes.post('/users', authMiddleware, UserController.store);
 routes.put('/users/:id', authMiddleware, UserController.update);
 routes.delete('/users/:id', authMiddleware, UserController.destroy);
 
