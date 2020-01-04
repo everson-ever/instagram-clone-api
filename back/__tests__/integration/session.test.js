9c4a8e5fc607c3eef7b51a6dd12e83d80a4e2000
Everson Silva <eversonsilva9799@gmail.com>
2020-01-04 20:02:27 -0300

    add test to user with incorrect data login

--- back/__tests__/integration/session.test.js
+++ back/__tests__/integration/session.test.js
@@ -4,12 +4,10 @@ const port = process.env.PORT || 3333;
 const api = `http://localhost:${port}/api`;
 
 describe('session', () => {
-
-
-	it('POST /session', () => {
+	it('POST /session expected token in response', () => {
 		return request(api)
 			.post('/session')
-			.send({ email: 'lourdessadasd@gmail.com', password: '111111' })
+			.send({ email: 'everson@mail.com', password: '000000' })
 			.then((response) => {
 				expect(response.status).toBe(200);
 				expect(response.body.token).toBeDefined();
@@ -17,6 +15,14 @@ describe('session', () => {
 			.catch(fail);
 	});
 
-
-
+	it("POST /session don't expected token in response", () => {
+		return request(api)
+			.post('/session')
+			.send({ email: 'everson@mail.com', password: '111111' })
+			.then((response) => {
+				expect(response.status).toBe(404);
+				expect(response.body.token).toBeUndefined();
+			})
+			.catch(fail);
+	});
 });
