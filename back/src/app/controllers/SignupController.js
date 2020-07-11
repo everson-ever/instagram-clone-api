587584230f27437ee4c3b63e04fbe3caada5d8e3
Everson Silva <eversonsilva9799@gmail.com>
2020-07-11 17:52:10 -0300

    refactor

--- back/src/app/controllers/SignupController.js
+++ back/src/app/controllers/SignupController.js
@@ -9,16 +9,17 @@ class SignupController {
             const fields = req.body;
             const { email } = req.body;      
             const requiredFields = ['name', 'email', 'gender', 'password'];
-            const errors = UserService.checkRequiredParams(fields, requiredFields);
+            const missingParams = UserService.checkRequiredParams(fields, requiredFields);
 
-            if (errors.length) {
-                return res.status(404).json(badRequest(new MissingParamError(errors)));
+            if (missingParams) {
+                return res.status(400).json(badRequest(new MissingParamError(missingParams)));
             }
 
             let userExists = await UserService.findByEmail(email);
-            const error = ['Este E-email já foi cadastrado no sistema']
-            if (userExists)
+            if (userExists) {
+                const error = ['Este E-email já foi cadastrado no sistema'];
                 return res.status(400).json(badRequest(new MissingParamError(error)));
+            }
 
 			let user = await UserService.create(fields);
             
